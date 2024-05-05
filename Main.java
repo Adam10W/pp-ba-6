a8f4ccabce059b77b4e979a5c3785b708af6901a
Adam <wieckowskiadam10@gmail.com>
2024-05-05 10:07:01 +0200

    dodane-zad4

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
 import company.models.Manager;
 import company.models.Worker;
 import company.abstracts.Employee;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -8,18 +10,34 @@ public class Main {
         Worker worker0 = new Worker("Andrzej", 4900, 0, "11-12-2022", "Sprzedawca");
         Worker worker1 = new Worker("Krystyna", 3500, 2, "30-06-2019", "Konsultant");
         Worker worker2 = new Worker("Bożena", 7300, 2, "09-02-2021", "Office");
+        Worker worker3 = new Worker("Henia", 4300, 5, "02-02-2021", "Kierowca");
+        Worker worker4 = new Worker("Ania", 3300, 6, "11-12-2020", "Staż");
 
-        Manager manager = new Manager("Adam", 14500, 5, "10-08-2018", "Manager");
+        Manager manager0 = new Manager("Adam", 14500, 9, "10-08-2018", "Manager");
+        Manager manager1 = new Manager("Magda", 11500, 8, "22-07-2017", "Junior-Manager");
 
-        System.out.println(worker0.getName() + " has code: " + worker0.hashCode());
-        System.out.println(worker1.getName() + " has code: " + worker1.hashCode());
-        System.out.println(worker2.getName() + " has code: " + worker2.hashCode());
-        System.out.println(manager.getName() + " has code: " + manager.hashCode());
+ List<Employee> employees = new ArrayList<>();
+        employees.add(worker0);
+        employees.add(worker1);
+        employees.add(worker2);
+        employees.add(worker3);
+        employees.add(worker4);
+        employees.add(manager0);
+        employees.add(manager1);
 
-        for (Employee employee : List.of(worker0, worker1, worker2, manager)) {
-            if (worker2.equals(employee)) {
-                System.out.println(worker2.getName() + " is equal to " + employee.getName());
-            }
-        }
+        double salaryAllEmployees = employees.stream().mapToDouble(Employee::getSalary).sum();
+        System.out.println("Zarobki wszystkich: " + salaryAllEmployees);
+
+        double salaryAllManagers = employees.stream()
+                .filter(employee -> employee instanceof Manager)
+                .mapToDouble(Employee::getSalary)
+                .sum();
+        System.out.println("Zarobki managerow: " + salaryAllManagers);
+
+        double salaryAllWorkers = employees.stream()
+                .filter(employee -> employee instanceof Worker)
+                .mapToDouble(Employee::getSalary)
+                .sum();
+        System.out.println("Zarobki pracowników: " + salaryAllWorkers);    
     }
 }
